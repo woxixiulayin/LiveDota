da74611c7f4b0c4d40251481ba4a2bd13fc23fb2
Jackson Liu <416883813@qq.com>
2017-01-11 23:42:17 +0800

    continer中添加对props是否有变化的判断

--- assets/js/lib/container/index.jsx
+++ assets/js/lib/container/index.jsx
@@ -1,7 +1,7 @@
-import {Component} from 'react'
+import { Component } from 'react'
 import isEqual from 'lodash/isEqual'
 
-const mergeProps = (stateProps={}, dispatchProps={}, parentProps={}) => ({
+const mergeProps = (stateProps = {}, dispatchProps = {}, parentProps = {}) => ({
     ...parentProps,
     ...stateProps,
     ...dispatchProps,
@@ -12,25 +12,37 @@ export default function continer(mapStatToProps, mapDispatchTopros, updateRefer)
     return class extends SourceComponent {
 
         _onChange(state) {
-            let props = mapStatToProps(state)
-            
-            this.setState(props)
+            let props = mapStatToProps(state),
+                _props = {},
+                shouldSetState = false
+
+            Object.keys(props).map(item => {
+                if (typeof props[item] !== 'undefined') {
+                    if (!shouldSetState) {
+                        shouldSetState = true
+                    }
+                    _props[item] = state[item]
+                }
+            })
+            if(shouldSetState) {
+                this.setState(_props)
+            }
         }
 
         componentDidMount() {
             let {store, mapStatToProps, mapDispatchTopros} = this.props.store
-            
-            if(!store) {
+
+            if (!store) {
                 throw new Error('container component should be given a store prop')
             }
 
             //不映射state和dispatch的组件不需要当做container
-            if(!mapStatToProps || !mapDispatchTopros) {
+            if (!mapStatToProps || !mapDispatchTopros) {
                 throw new Error('container component should set mapStatToProps or mapDispatchTopros')
             }
 
             //将dispatch方法加到组件上
-            if(mapDispatchTopros) {
+            if (mapDispatchTopros) {
                 Object.assign(this, mapDispatchTopros)
             }
 
