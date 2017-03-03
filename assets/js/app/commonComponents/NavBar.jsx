d2dd5a7cc95ac8a891b938727185d6c1c03aac8b
Jackson Liu <416883813@qq.com>
2017-03-03 23:09:48 +0800

    修改navbar hover样式

--- assets/js/app/commonComponents/NavBar.jsx
+++ assets/js/app/commonComponents/NavBar.jsx
@@ -15,46 +15,62 @@ class NavBar extends PureComponent {
     constructor() {
         super()
         this.state = {
-            hintBorderLeft: 0,
-            hintBorderTop: 0,
-            hintBorderWidth: 0,
+            hoverIndex: -1
         }
+        this.items = []
     }
 
-    getItemNodeInfo(item) {
-        let info = {}
-
-        let node = this[`${item}`],
+    getItemLayoutInfo(index) {
+        let node = this.items[index],
             width = node.clientWidth - 10,
             left = node.offsetLeft + 3,
             top = node.offsetTop + 22
-
-        info = {width, left , top}
-        return info
+ 
+        return {width, left , top}
     }
 
-    setBorderStyle() {
-        let style = this.getItemNodeInfo(this.props.items[this.props.currentIndex]),
+    getIndexOfItemToShow() {
+        let {hoverIndex} = this.state,
+            {currentIndex} = this.props
+
+        return hoverIndex === -1 ? currentIndex : hoverIndex 
+    }
+
+    setBorderLayout() {
+        let layoutInfo = this.getItemLayoutInfo(this.getIndexOfItemToShow()),
             borderStyle = this.hintBorder.style
-        borderStyle.left = style.left + 'px';
-        borderStyle.top = style.top + 'px';
-        borderStyle.width = style.width + 'px';
+        borderStyle.left = layoutInfo.left + 'px';
+        borderStyle.top = layoutInfo.top + 'px';
+        borderStyle.width = layoutInfo.width + 'px';
     }
+
     changeIndex(index) {
         this.props.changeIndex(index)
     }
 
     componentDidMount() {
-        // this.setState(this.getCategoryNodeInfo(this.props.categorys[this.props.currentIndex]))
-        this.setBorderStyle()
+        this.setBorderLayout()
     }
 
-    shouldComponentUpdate(nextProps) {
-        return nextProps.currentIndex !== this.props.currentIndex
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.currentIndex !== this.props.currentIndex ||
+            nextState.hoverIndex !== this.state.hoverIndex
     }
 
     componentDidUpdate() {
-        this.setBorderStyle()
+        this.setBorderLayout()
+    }
+
+    onItemMouseEnter(index) {
+        this.setState({
+            hoverIndex: index
+        })
+    }
+
+    onItemMouseOut() {
+        this.setState({
+            hoverIndex: -1
+        })
     }
 
     render() {                                        
@@ -64,14 +80,13 @@ class NavBar extends PureComponent {
             {items.map( (item, index) => {
                 return <span
                 ref={node => {
-                    this[`${item}`] = node
+                    this.items[index] = node
                 }}
                 key={index}
-                onClick={() => {
-                        this.changeIndex(index)
-                    }
-                 }
-                className={classNames("header-category main-font", {'current': +currentIndex === index})}
+                onClick={this.changeIndex.bind(this, index)}
+                onMouseEnter={this.onItemMouseEnter.bind(this, index)}
+                onMouseOut={this.onItemMouseOut.bind(this, index)}
+                className={classNames("header-category", {'current': +currentIndex === index})}
                     >
                     {item}
                 </span>
