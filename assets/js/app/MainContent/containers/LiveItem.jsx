744ccb6953807523d03e80a4b71654bb804e8ebd
Jackson Liu <416883813@qq.com>
2017-03-08 19:54:20 +0800

    live-item布局

--- assets/js/app/MainContent/containers/LiveItem.jsx
+++ assets/js/app/MainContent/containers/LiveItem.jsx
@@ -1,21 +1,46 @@
-import React,{Component, PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
 import classnames from 'classnames'
 
-// var liveSchema = mongoose.Schema({
-//     name: {type:String, unique:true},
-//     nums: Number,
-//     title: String,
-//     link: String,
-//     category: String,
-//     img: String,
-//     website: String
-// }
+//  "name": "灰灰๑",     "nums": "629",     "title": "\r\n        小奶：单排冲8000！
+//                   ", "link": "http://www.douyu.com/xiaonai",     "category":
+// "",     "img":
+// "https://rpic.douyucdn.cn/a1610/24/20/224460_161024205221.jpg", "website":
+// "斗鱼"
 
 export default class LiveItem extends Component {
+    static propTypes = {
+        live: PropTypes.object.isRequired
+    }
+
+    static defaultProps = {
+        live: {
+                name: '灰灰',
+                nums: '629',
+                title: '\r\n                                                        小奶：单排冲8000！         ' +
+                        '               ',
+                link: 'http://www.douyu.com/xiaonai',
+                img: 'https://rpic.douyucdn.cn/a1610/24/20/224460_161024205221.jpg'
+            }
+    }
+    
+    constructor() {
+        super()
+    }
+
     render() {
+        let {name, nums, title, link, img} = this.props.live
+
         return (
             <div className="live-item">
-
+                <div className="video-cover">
+                    <img src={img} alt="" className="video-img"/>
+                    <div className="play-mask"></div>
+                </div>
+                <div className="video-title">{title}</div>
+                <div className="video-info">
+                    <span className="video-nickname">{name}</span>
+                    <span className="video-nums">{nums}</span>
+                </div>
             </div>
         )
     }
